552a12b54c847dea4485a6152f1aa15958d1066c
Leonardo Abdalla <79482146+leonardoabdalla@users.noreply.github.com>
2021-06-23 21:15:52 -0300

    Update index.py

--- Calculadora/index.py
+++ Calculadora/index.py
@@ -2,39 +2,48 @@ import math
 entrada = input('Deseja utilizar a calculadora? (S/N) ')
 entrada = entrada.upper()
 while ( entrada == 'S'):
-    entrada = input('Deseja realizar operação simples ou equação do segundo grau? (simples/equação) ')
+    print(' 1 - Simples\n 2 - Equação do segundo grau')
+    entrada = input('Deseja realizar qual operação? ')
     entrada = entrada.lower()
-    if entrada == 'equação':
+    if entrada == '2':
         a = int(input('Digite numero de A(x2): '))
         b = int(input('Digite o numero de B(x): '))
         c = int(input('digite o numero de C:'))
         x1 = (-b + ((b ** 2 - 4 * a * c) ** 0.5)) / 2 * a
         x2 = (-b - ((b ** 2 - 4 * a * c) ** 0.5)) / 2 * a
         print(x1, x2)
-    elif entrada == 'simples':
+    elif entrada == '1':
         numeroA = int(input('Digite o primeiro numero: '))
-        operacao = input('Escolha  operação desejada(+ ; - ; / ; * ; % ; ! ; sen ; cos ; tang) :')
-        if operacao == '!':
+        print(' 1 = Soma\n 2 = Subtração\n 3 = Divisão\n 4 = Multiplicação\n 5 = Porcentgem\n'' 6 = Fatorial\n '
+              '7 = seno\n 8 = cosseno''\n 9 = tangente\n 10 = Raiz quadrada\n 11 = Binário\n 12 = Hexal')
+        operacao = input('Escolha operação desejada: ')
+        if operacao == '6':
             print('Resultado do fatorial de {} é {}'.format(numeroA, math.factorial(numeroA)))
-        elif operacao == 'sen':
+        elif operacao == '7':
             print('Resultado do seno de {} é {}'.format(numeroA, math.sin(numeroA)))
-        elif operacao == 'cos':
+        elif operacao == '8':
             print('Resultado do cosseno de {} é {}'.format(numeroA, math.cos(numeroA)))
-        elif operacao == 'tang':
+        elif operacao == '9':
             print('Resultado do tangente de {} é {}'.format(numeroA, math.tan(numeroA)))
-        elif operacao == 'raiz':
+        elif operacao == '10':
             print('Resultado da raiz quadrada de {} é {}'.format(numeroA, math.sqrt(numeroA)))
+        elif operacao == '11':
+            print('O número {} convertido para binário é {}'.format(numeroA, bin(numeroA)))
+        elif operacao == '10':
+            print('O número {} convertido para octal é {}'.format(numeroA, oct(numeroA)))
+        elif operacao == '10':
+            print('O número {} convertido pra hexal é {}'.format(numeroA, hex(numeroA)))
         else:
             numeroB = int(input('Digite mais um numero: '))
-            if operacao == '+':
+            if operacao == '1':
                 print('Resultado da soma: ', numeroA + numeroB)
-            elif operacao == '-':
+            elif operacao == '2':
                 print('Resultado da subtração: ', numeroA - numeroB)
-            elif operacao == '/':
+            elif operacao == '3':
                 print('Resultado da divisão: ', numeroA / numeroB)
-            elif operacao == '*':
+            elif operacao == '4':
                 print('Resultado da multiplicação: ', numeroA * numeroB)
-            elif operacao == '%':
+            elif operacao == '5':
                 print(numeroA, 'porcentos de ', numeroB, 'é: ', numeroA*numeroB/100)
             else:
                 print('Operador não identificado')
